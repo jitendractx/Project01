a25bd716a13392c3c0c6e68e6c9a8ed3782477b2
jitendra <mail@mail.com>
2025-04-17 17:08:43 +0530

    Revert "WIP01" revert This reverts commit 09ef052b0638a00912a6ed5ffd3856b044f2bacc.

--- main.tf
+++
@@ -1,7 +0,0 @@
-resource "github_repository" "git_repo_01" {
-  name        = var.repo_001
-  description = "Git Action Repo"
-  visibility = "public"
-  auto_init = true
-
-}
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-repo_001 = "Project-001"
-github_token = "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"
